11e1c87ce7185585e69dabd7190c4ce7e5a779b3
Senad Hajric <senadhajric3@gmail.com>
2024-10-14 09:19:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# foodie_jp_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/foodie_jp_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodie_jp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
